7c8d97b880e33a0d388c28eb237a3bc6611dd877
nexweb1 <nexweb1@gmail.com>
2020-03-06 08:52:19 +0900

    Modify Customer Controller

--- src/main/java/com/ibm/lab/customer/controller/CustomerController.java
+++ src/main/java/com/ibm/lab/customer/controller/CustomerController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -53,6 +54,7 @@ public class CustomerController {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@PutMapping(value="/{id}")
 	public ResponseEntity<Object> update(@RequestBody CustomerDTO custDto, @PathVariable String id) {
 		Customer cust = customer.fromDTO(custDto);
 		cust.setId(id);
